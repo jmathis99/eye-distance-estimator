5b3fae64d7f87fc3bc0bc5f5584b09b5404aa765
jmathis99 <joseph.e.mathis@vanderbilt.edu>
2020-04-28 12:16:09 -0500

    finish documentation

--- constants.py
+++ constants.py
@@ -6,8 +6,6 @@ Set of constants for use in the eye distance estimator
 # Constants for calibration
 CALIB_LOC = 'calibrationdata.pickle'
 
-# Constants for eye detection
-
 # Constants for distance estimation
 EYE_WIDTH = 24.22 # mm, transverse plane
 EYE_HEIGHT = 23.7 # mm, sagittal plane
